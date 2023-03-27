607356e7c1a5936f185c131e09f6c67012159a2d
JUAN VITOR TASCA DE ANDRADE <juan.4910@aluno.pr.senac.br>
2023-03-27 07:57:51 -0300

    baababab

--- cadastrar.php
+++ cadastrar.php
@@ -5,12 +5,36 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="meuestilo.css">
         <title>Cadastro</title>
     </head>
     <body>
         <div class="container">
-                 <h1>Cadastro de Pessoas</h1>
+            <h1>Tela de cadastro de clientes</h1>
+        </div>
+        <div class="container">
+            <form action="">
+                <div class="mb-3">
+                    <label for=""> Nome: </label>
+                    <input type="text">
+                </div>
+                <div class="mb-3">
+                    <label for=""> Endereço: </label>
+                    <input type="text">
+                </div>
+                <div class="mb-3">
+                    <label for=""> Estado: </label>
+                    <input type="text">
+                </div>
+                <div class="mb-3">
+                    <label for=""> País: </label>
+                    <input type="text">
+                </div>
+                <div class="mb-3">
+                    <input type="submit" class="btn btn-success" value="Enviar">
+                    <input type="reset" class="btn btn-danger" value="Cancelar">
+                </div>
+            </form>
         </div>
     </body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
